a74c959f7125659eacfda9777095a562e3793e8b
Rogerio Junior <rjunior@mit.edu>
2020-05-12 02:35:43 -0400

    Implemented a transformer that can translate whole tweets. Still need to implement training

--- twitterCycleGAN.py
+++ twitterCycleGAN.py
@@ -1,18 +1,20 @@
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 ###################### USED IN DATA PREPARATION ######################
 import torchtext
 from torchtext.data.utils import get_tokenizer
+import re
 ######################################################################
 
 import time
 import pandas as pd
+import numpy as np
 
 import json
-
+import os
+import csv
 import sys
 
 class PositionalEncoding(nn.Module):
@@ -37,88 +39,185 @@ class PositionalEncoding(nn.Module):
 
 class Transformer(nn.Module):
 
-    def __init__(self, n_words, embedded_size, n_heads, n_hidden, n_layers, dropout=0.5, device='cuda'):
+    def __init__(self, input_vocab_size, output_vocab_size, embedded_size, n_heads, n_hidden, n_layers,
+                 dropout=0.5, device=torch.device('cuda'), max_len=50, pad=0, sos=1, eos=2):
         super(Transformer, self).__init__()
 
         self.device = device
         self.model_type = 'Transformer'
-        self.src_mask = None
+        self.sos = sos
+        self.eos = eos
+        self.max_len = max_len
+
+        self.embedded_size = embedded_size
+        self.input_vocab_size = input_vocab_size
+        self.src_encoder = nn.Embedding(input_vocab_size, embedded_size, padding_idx=pad)
+
         self.positional_encoder = PositionalEncoding(embedded_size, dropout)
 
         encoder_layers = nn.TransformerEncoderLayer(embedded_size, n_heads, n_hidden, dropout)
 
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, n_layers)
-        self.encoder = nn.Embedding(n_words, embedded_size)
 
-        self.embedded_size = embedded_size
-        self.n_words = n_words
+        decoder_layers = nn.TransformerDecoderLayer(embedded_size, n_heads, n_hidden, dropout)
 
-        self.decoder = nn.Linear(embedded_size, n_words)
+        self.trg_encoder = nn.Embedding(output_vocab_size, embedded_size, padding_idx=pad)
+
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layers, n_layers)
+
+        self.trg_decoder = nn.Linear(embedded_size, output_vocab_size)
 
         self.init_weights()
 
 
-    def _generate_square_mask_(self, size):
-        mask = torch.triu(torch.ones(size, size) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    def init_weights(self):
+        init_range = 0.1
+        self.src_encoder.weight.data.uniform_(-init_range, init_range)
+        self.trg_encoder.weight.data.uniform_(-init_range, init_range)
+        self.trg_decoder.bias.data.zero_()
+        self.trg_decoder.weight.data.uniform_(-init_range, init_range)
+
+
+    def _generate_padding_mask_(self, batch_size, max_len, trg_lens):
+        mask = torch.ones(batch_size, max_len, dtype=torch.bool)
+        for idx, trg_len in enumerate(trg_lens):
+            mask[idx][:trg_len] = False
         return mask
 
 
-    def init_weights(self):
-        init_range = 0.1
-        self.encoder.weight.data.uniform_(-init_range, init_range)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-init_range, init_range)
-
-
-    def forward(self, src):
-        if self.src_mask is None or self.src_mask.size(0) != len(src):
-            device = src.device
-            mask = self._generate_square_mask_(len(src)).to(device)
-            self.src_mask = mask
-
-        src_embeddings = self.encoder(src) * math.sqrt(self.embedded_size)
+    def forward(self, src, trg, src_lens, trg_lens):
+        src_embeddings = self.src_encoder(src) * math.sqrt(self.embedded_size)
         src_positional_embeddings = self.positional_encoder(src_embeddings)
-        output_embeddings = self.transformer_encoder(src_positional_embeddings)
-        output = self.decoder(output_embeddings)
+
+        max_len, batch_size = list(src.size())
+        src_padding_mask = self._generate_padding_mask_(batch_size, max_len, src_lens).to(self.device)
+        memory = self.transformer_encoder(src_positional_embeddings, src_key_padding_mask=src_padding_mask)
+
+        trg_embeddings = self.trg_encoder(trg) * math.sqrt(self.embedded_size)
+        trg_positional_embeddings = self.positional_encoder(trg_embeddings)
+
+        trg_padding_mask = self._generate_padding_mask_(batch_size, max_len, trg_lens).to(self.device)
+
+        output_embeddings = self.transformer_decoder(trg_positional_embeddings, memory,
+                                                     memory_key_padding_mask=src_padding_mask,
+                                                     tgt_key_padding_mask=trg_padding_mask,)
+        output = self.trg_decoder(output_embeddings)
+
         return output
 
+    def translate_tweet(self, src, src_lens):
+        max_len, batch_size = list(src.size())
 
-def train_model(model_name, device):
+        trg = torch.zeros(max_len, batch_size, dtype=torch.long).to(self.device)
+        trg[0,:] = self.sos
+
+        trg_lens = torch.ones(batch_size, dtype=torch.long).to(self.device)
+        is_finished = torch.zeros(batch_size, dtype=torch.bool).to(self.device)
+
+        for idx in range(1, max_len-1):
+            output = self.forward(src, trg, src_lens, trg_lens)
+            pred = output[idx, :, :].max(1).indices
+            finished_now = (pred == self.eos)
+            trg_lens[finished_now] = idx+1
+            is_finished = is_finished | finished_now
+            trg[idx][~is_finished] = pred[~is_finished]
+
+        trg[max_len-1][~is_finished] = self.eos
+        trg_lens[~is_finished] = max_len
+
+        return trg, trg_lens
+
+def train_model(model_name, user1, user2, device):
 
     ################### prepare the data ###################
 
-    def prepare_data(batch_size, eval_batch_size, device='cuda'):
-        TEXT = torchtext.data.Field(tokenize=get_tokenizer("basic_english"),
-                                    init_token='<sos>', eos_token='<eos>', lower=True)
-        train_text, val_txt, test_txt = torchtext.datasets.WikiText2.splits(TEXT)
-        TEXT.build_vocab(train_text)
+    def prepare_data(user, batch_size, max_len, device):
+        train_df = pd.read_csv('data/' + user + '/train.csv')
+        val_df = pd.read_csv('data/' + user + '/val.csv')
+        test_df = pd.read_csv('data/' + user + '/test.csv')
 
-        def batchify(data, batch_size):
-            data = TEXT.numericalize([data.examples[0].text])
-            n_batches = data.size(0) // batch_size
-            data = data.narrow(0, 0, n_batches * batch_size)
-            data = data.view(batch_size, -1).t().contiguous()
-            return data.to(device)
+        vocab_itos = {0: '<pad>', 1: '<sos>', 2: '<eos>', 3: '<unk>'}
+        vocab_stoi = {'<pad>': 0, '<sos>': 1, '<eos>': 2, '<unk>': 3}
 
-        train_data = batchify(train_text, batch_size)
-        val_data = batchify(val_txt, eval_batch_size)
-        test_data = batchify(test_txt, eval_batch_size)
+        train_tokens = torch.zeros(len(train_df), max_len, dtype=torch.long)
+        val_tokens = torch.zeros(len(val_df), max_len, dtype=torch.long)
+        test_tokens = torch.zeros(len(test_df), max_len, dtype=torch.long)
 
-        n_words = len(TEXT.vocab.stoi)
+        train_lens = torch.zeros(len(train_df), dtype=torch.long)
+        val_lens = torch.zeros(len(val_df), dtype=torch.long)
+        test_lens = torch.zeros(len(test_df), dtype=torch.long)
 
-        return train_data, val_data, test_data, n_words
+        for i, tweet in enumerate(train_df['text']):
+            words = tweet.split(' ')
+            if len(words) > max_len - 2:
+                words = words[:max_len - 2]
+            words = ['<sos>'] + words + ['<eos>']
+            train_lens[i] = len(words)
+            for j, word in enumerate(words):
+                if word not in vocab_stoi:
+                    token = len(vocab_stoi)
+                    vocab_stoi[word] = token
+                    vocab_itos[token] = word
+                else:
+                    token = vocab_stoi[word]
+                train_tokens[i][j] = token
 
-    bptt = 35
-    def get_batch(source, i):
-        seq_len = min(bptt, len(source) - 1 - i)
-        data = source[i:i + seq_len]
-        target = source[i + 1:i + 1 + seq_len].view(-1)
-        return data, target
+        for i, tweet in enumerate(val_df['text']):
+            words = tweet.split(' ')
+            if len(words) > max_len - 2:
+                words = words[:max_len - 2]
+            words = ['<sos>'] + words + ['<eos>']
+            val_lens[i] = len(words)
+            for j, word in enumerate(words):
+                if word not in vocab_stoi:
+                    token = vocab_stoi['<unk>']
+                else:
+                    token = vocab_stoi[word]
+                val_tokens[i][j] = token
 
+            for i, tweet in enumerate(test_df['text']):
+                words = tweet.split(' ')
+                if len(words) > max_len - 2:
+                    words = words[:max_len - 2]
+                words = ['<sos>'] + words + ['<eos>']
+                test_lens[i] = len(words)
+                for j, word in enumerate(words):
+                    if word not in vocab_stoi:
+                        token = vocab_stoi['<unk>']
+                    else:
+                        token = vocab_stoi[word]
+                    val_tokens[i][j] = token
+
+        def batchify(tokens, lens, batch_size):
+            n_batches = tokens.size(0) // batch_size
+            data = tokens.narrow(0, 0, n_batches * batch_size)
+            lens = lens.narrow(0, 0, n_batches*batch_size)
+            data = data.view(n_batches, batch_size, -1).transpose(1,2).contiguous()
+            lens = lens.view(n_batches, batch_size).contiguous()
+            return data.to(device), lens.to(device)
+
+        train_data, train_lens = batchify(train_tokens, train_lens, batch_size)
+        val_data, val_lens = batchify(val_tokens, val_lens, batch_size)
+        test_data, test_lens = batchify(test_tokens, test_lens, batch_size)
+
+        return train_data, val_data, test_data, train_lens, val_lens, test_lens, vocab_itos
     ################### training settings ###################
 
-    train_data, val_data, test_data, n_words = prepare_data(batch_size=20, eval_batch_size=10, device=device)
+    batch_size = 20
+    max_len = 50
+
+    (user1_train_data, user1_val_data, user1_test_data,
+     user1_train_lens, user1_val_lens, user1_test_lens, user1_vocab_itos) = prepare_data(user1, batch_size=batch_size,
+                                                                                       max_len=max_len, device=device)
+
+    (user2_train_data, user2_val_data, user2_test_data,
+     user2_train_lens, user2_val_lens, user2_test_lens, user2_vocab_itos) = prepare_data(user2, batch_size=batch_size,
+                                                                                       max_len=max_len, device=device)
+
+    print(user1_train_data.size())
+
+    user1_vocab_size = len(user1_vocab_itos)
+    user2_vocab_size = len(user2_vocab_itos)
 
     embedded_size = 200
     n_heads = 2
@@ -126,8 +225,26 @@ def train_model(model_name, device):
     n_layers = 2
     dropout = 0.2
 
-    model = Transformer(n_words, embedded_size, n_heads, n_hidden, n_layers, dropout).to(device)
+    model = Transformer(user1_vocab_size, user2_vocab_size, embedded_size, n_heads,
+                        n_hidden, n_layers, dropout, device=device).to(device)
 
+    example = user1_train_data[0,:,:1]
+    example_len = user1_train_lens[0,:1]
+    output_example, output_len = model.translate_tweet(example, user1_train_lens[0,:1])
+
+    print(example)
+    print(list(map(lambda x: user1_vocab_itos[x.item()], example[:example_len.item()])))
+
+
+    print(output_example)
+    print(output_example.size())
+    print(output_len)
+    output_sentence = list(map(lambda x: user2_vocab_itos[x.item()], output_example[:output_len.item()]))
+    print(len(output_sentence))
+    print(output_sentence)
+
+
+    return None
     criterion = nn.CrossEntropyLoss()
     lr = 5.0
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
@@ -205,12 +322,13 @@ def train_model(model_name, device):
 
     model_params = model.state_dict()
 
-    model_kwargs = {'n_words': n_words, 'embedded_size': embedded_size, 'n_heads': n_heads, 'n_hidden': n_hidden,
-              'n_layers': n_layers, 'dropout': dropout}
+    model_kwargs = {'input_vocab_size': user1_vocab_size, 'output_vocab_size': user2_vocab_size,
+                    'embedded_size': embedded_size, 'n_heads': n_heads, 'n_hidden': n_hidden, 'n_layers': n_layers,
+                    'dropout': dropout, 'device': device}
 
     save_model(model_name, model_params, model_kwargs)
 
-    return best_model
+    return best_model, None
 
 def save_model(model_name, model_params, model_kwargs):
 
@@ -250,23 +368,88 @@ def test_model(model_name, device):
 
     print(model.state_dict())
 
+
+def prepare_text(s):
+    words = s.split(' ')
+    websites = []
+    for word in words:
+        if 'http' in word:
+            websites.append(word)
+    for website in websites:
+        words.remove(website)
+    s = " ".join(words)
+    words = re.split(r'(\W)', s)
+    n_spaces, n_blanks, n_breaks, n_r = 0, 0, 0, 0
+    for word in words:
+        if word == ' ':
+            n_spaces += 1
+        if word == '':
+            n_blanks += 1
+        if word == '\n':
+            n_breaks += 1
+        if word == '\r':
+            n_r += 1
+    for space in range(n_spaces):
+        words.remove(" ")
+    for blank in range(n_blanks):
+        words.remove('')
+    for line_break in range(n_breaks):
+        words.remove('\n')
+    for r in range(n_r):
+        words.remove('\r')
+    s = " ".join(words)
+    return s
+
+def prepare_user_data(user, n_tweets):
+
+    dataset = pd.read_csv('data/dataset.csv', lineterminator='\n')
+
+    data = dataset[dataset['user'] == user]['text']
+
+    data = pd.Series(map(prepare_text, data))
+    data = pd.DataFrame(data, columns=['text'])
+
+    data['text'].replace('', np.nan, inplace=True)
+    data.dropna(subset=['text'], inplace=True)
+
+    data = data[:n_tweets]
+
+    data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+    pct_train, pct_val = (0.8, 0.1)
+    n_train = math.floor(n_tweets * pct_train)
+    n_val = math.floor(n_tweets * pct_val)
+
+    data_train = data[:n_train].reset_index()
+    data_val = data[n_train:n_train + n_val].reset_index()
+    data_test = data[n_train + n_val:].reset_index()
+
+    os.mkdir('data/' + user)
+
+    data_train.to_csv('data/' + user + '/train.csv', index_label='index')
+    data_val.to_csv('data/' + user + '/val.csv', index_label='index')
+    data_test.to_csv('data/' + user + '/test.csv', index_label='index')
+
+
 if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     device = torch.device(device)
     print('device is:', device)
 
-    #dataset_path = 'data/dataset.csv'
-    #dataset = pd.read_csv(dataset_path)
-
     mode = sys.argv[1]
 
     if mode == 'train':
-        model_name = sys.argv[2]
-        model = train_model(model_name=model_name, device=device)
+        model_name, user1, user2 = sys.argv[2], sys.argv[3], sys.argv[4]
+        model1, model2 = train_model(model_name=model_name, user1=user1, user2=user2, device=device)
 
     if mode == 'test':
         model_name = sys.argv[2]
         test_model(model_name=model_name, device=device)
+
+    if mode == 'prepare_user_data':
+        user = sys.argv[2]
+        n_tweets = int(sys.argv[3])
+        prepare_user_data(user, n_tweets)
 
 
